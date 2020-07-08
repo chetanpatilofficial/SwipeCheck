f9c22a1a95afb7fc3104ff5df00a81d925b08a97
Chetan Patil <43129505+chetanpatilofficial@users.noreply.github.com>
2020-07-08 02:25:50 -0400

    Update kafka_setup.sh

--- bash_scripts/kafka_setup.sh
+++ bash_scripts/kafka_setup.sh
@@ -16,9 +16,9 @@ peg install ${CLUSTER_NAME} aws
 peg install ${CLUSTER_NAME} environment
 
 # install and unzip confluent kafka files
-peg sshcmd-cluster ${CLUSTER_NAME} "curl -O https://packages.confluent.io/archive/5.5/confluent-5.5.0-2.12.tar.gz"
-peg sshcmd-cluster ${CLUSTER_NAME} "tar -zxf confluent-5.5.0-2.12.tar.gz"
-peg sshcmd-cluster ${CLUSTER_NAME} "mv confluent-5.5.0 confluent"
+peg sshcmd-cluster ${CLUSTER_NAME} "curl -O http://packages.confluent.io/archive/5.5/confluent-5.5.1-2.12.zip"
+peg sshcmd-cluster ${CLUSTER_NAME} "tar -zxf confluent-5.5.1-2.12.tar.gz"
+peg sshcmd-cluster ${CLUSTER_NAME} "mv confluent-5.5.1 confluent"
 
 # install python libraries
 peg sshcmd-cluster ${CLUSTER_NAME} "pip install faker"
