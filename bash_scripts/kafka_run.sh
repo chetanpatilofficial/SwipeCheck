018e020cc7e18483e7ba5ad1d4c07b32d7069499
Chetan Patil <43129505+chetanpatilofficial@users.noreply.github.com>
2020-07-08 02:24:14 -0400

    updated scripts to add updated python file name

--- bash_scripts/kafka_run.sh
+++ bash_scripts/kafka_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python src/kafka/transaction_user_location_producer.py
+python src/kafka/stream_producer.py
 
-python src/kafka/transaction_user_location_consumer.py
+python src/kafka/stream_consumer.py
